d79911b66789b1f10325890e20c4fbd7affdd9c8
mikelesnr <mwanza.n.m@gmail.com>
2024-07-16 18:07:41 +0200

    login api route created

--- auth-api/app/Http/Controllers/Api/ApiController.php
+++ auth-api/app/Http/Controllers/Api/ApiController.php
@@ -47,6 +47,55 @@ class ApiController extends Controller
             ], 500);
         }
     }
+
+    public function login(Request $request)
+    {
+        try {
+            $validateuser = Validator::make($request->all(), [
+                "email" => "required|email",
+                "password" => "required",
+            ]);
+
+            if ($validateuser->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => "validation error",
+                    'errors' => $validateuser->errors()
+                ], 401);
+            }
+
+            if (!Auth::attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'status' => false,
+                    'message' => "Email and password do not match with our records",
+                ], 401);
+            }
+
+            $user = User::where("email", $request->email)->first();
+            return response()->json([
+                'status' => true,
+                'message' => "User login successful",
+                'token' => $user->createToken('API TOKEN')->plainTextToken
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function profile(Request $request)
+    {
+        echo 'jsiajiosjoiji';
+        die;
+    }
+
+    // public function logout(Request $request)
+    // {
+    //     //pass
+    // }
 }
 
 //Register, Login, Profile and Logout
--- auth-api/routes/api.php
+++ auth-api/routes/api.php
@@ -5,10 +5,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApiController;
 
 //Register
-Route::post("register", [ApiController::class, "register"])->name("reg");
+Route::post("register", [ApiController::class, "register"]);
 
-Route::post("login", [ApiController::class, "login"])->name("log");
+//login
+Route::post("login", [ApiController::class, "login"]);
 
+Route::group([
+    "middleware" => ["auth:sanctum"],
+], function () {
+    //profile
+    Route::get("profile", [ApiController::class, "profile"])->name("prof");
+
+});
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
